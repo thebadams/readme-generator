81c06ddb6c50a993e0739a79aeaddc3d39ede89d
Brian P. Adams <bpeteradams@gmail.com>
2021-04-04 15:50:00 -0400

    Initial commit

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+import fs from "fs";
+import inquirer from "inquirer";
+
+console.log(inquirer)
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

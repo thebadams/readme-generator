cec7398098c29018bf0ed09207871406c96682ce
Brian Adams <bpeteradams@gmail.com>
2021-04-04 22:50:03 -0400

    some set up work

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,10 @@ import inquirer from "inquirer";
 console.log(inquirer)
 
 // TODO: Create an array of questions for user input
+//sections: Description, Table of Contents, Installation, Usage, License, Contributing, Tests, Questions
+
+// Project Title
+//
 const questions = [];
 
 // TODO: Create a function to write README file

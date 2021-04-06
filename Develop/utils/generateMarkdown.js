46c4e29728316fee524a55bb61816ceddcadb95c
Brian Adams <bpeteradams@gmail.com>
2021-04-06 15:21:12 -0400

    fleshed out generateMarkDown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,9 +11,32 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(ReadMeInfo) {
+  return `# ${ReadmeInfo.title}
+## Description
+${ReadmeInfo.description.description}
+- ${ReadMeInfo.description.motivation} 
+- ${ReadMeInfo.description.why}
+- ${ReadMeInfo.description.solves}
+- ${ReadMeInfo.description.learned}
+## Table of Contents
+If your README is long, add a table of contents to make it easy for users to find what they need.
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+${ReadMeInfo.instructions.installation}
+## Usage
+${ReadMeInfo.instructions.usage}
+## Credits
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.
+## License
+[![License: ${ReadMeInfo.license.short}](${ReadMeInfo.license.badgeURL})](${ReadMeInfo.license.linkURL})
 
+${ReadmeInfo.license.content}
 `;
 }
 

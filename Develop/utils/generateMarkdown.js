be0281dd1d92bf0ca8f316e4a5e1ec5ea2006af1
Brian Adams <bpeteradams@gmail.com>
2021-04-10 17:31:53 -0400

    wrote render license functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,16 +2,21 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `![${license.short}(${license.badgeURL})`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `![${license.short}](${license.URL})`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  return `${license.content}`
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(ReadMeInfo) {
   return `# ${ReadMeInfo.title}
